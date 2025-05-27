68c91361ed79c15222106aeef7ddd3dbe9124048
Arman Ali <arman@sourcedeskglobal.com>
2025-05-27 17:10:34 +0530

    Header review site logo list dynamic done. Solved loader logo issue problem. HeaderNav new file created.

--- app/components/Loader.jsx
+++ app/components/Loader.jsx
@@ -52,7 +52,7 @@ export default function Loader() {
   return (
     <div className={`custom-loading ${"show"}`}>
       <div className="spinner">
-        <img src="images/gwebpro_logo.png" alt="" />
+        <img src="/images/gwebpro_logo.png" alt="" />
         <div className="disc"></div>
       </div>
     </div>
